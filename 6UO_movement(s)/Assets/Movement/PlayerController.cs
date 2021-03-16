d3ef0ae37286c3d09d778c366597e66b8d409174
Little-White-PD <66425286+Little-White-PD@users.noreply.github.com>
2021-03-16 23:16:43 +0800

    Player 1 movement
    Player 1 movement without rotation and animation by using Input System.

---
+++ 6UO_movement(s)/Assets/Movement/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    private Vector2 movementInput;
+    public Vector2 rotateInput;
+
+    private void Update()
+    {
+        if (movementInput.x < 0)
+            GetComponent<Transform>().Rotate(Vector3);
+        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+    }
+
+        public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
+}
