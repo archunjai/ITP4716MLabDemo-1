072ce256254093d264f63e3aeef26da2a0403a1d
Little-White-PD <66425286+Little-White-PD@users.noreply.github.com>
2021-03-19 12:01:39 +0800

    Basic movement(Player 1)
    Player 1 can move and rotate 4 directions

--- 6UO_movement(s)/Assets/Movement/PlayerController.cs
+++ 6UO_movement(s)/Assets/Movement/PlayerController.cs
@@ -11,9 +11,29 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (movementInput.x > 0)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+            transform.Translate(new Vector3(movementInput.y, 0, movementInput.x) * speed * Time.deltaTime);
+        }
         if (movementInput.x < 0)
-            GetComponent<Transform>().Rotate(Vector3);
-        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+            transform.Translate(new Vector3(movementInput.y, 0, -movementInput.x) * speed * Time.deltaTime);
+        }
+
+        if (movementInput.y > 0)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+        }
+        if (movementInput.y < 0)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            transform.Translate(new Vector3(movementInput.x, 0, -movementInput.y) * speed * Time.deltaTime);
+        }
+            
+        
     }
 
         public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
